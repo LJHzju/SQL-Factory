2e6ece5d423edf7f027c35065dc0afb937fa090b
Jiahui Li <54147892+LJHzju@users.noreply.github.com>
2025-04-29 18:46:22 +0800

    Update Configuration

--- config/agent.py
+++ config/agent.py
@@ -13,7 +13,7 @@ llm_config: LLMConfig = {
         "model": "Qwen2.5-Coder-14B-Instruct",
         "api_key": "0",
         "base_url": "http://127.0.0.1:8000/v1",
-        "temperature": 0.0,
+        "temperature": 0.3,
         "top_p": 0.3,
         "max_tokens": 8192 * 2
     },
